97fb4b991c35e4b63feb3989519b609973a73507
josemlegal <josemlegal97@gmail.com>
2023-05-09 22:53:44 -0400

    Add locator

---
+++ lib/core/dependency_injection/locator.dart
@@ -0,0 +1,18 @@
+import 'package:get_it/get_it.dart';
+import 'package:prueba_itti/core/services/http_service.dart';
+import 'package:prueba_itti/data/repositories/user_repository_implementation.dart';
+import 'package:prueba_itti/domain/repostories/user_repository.dart';
+import 'package:stacked_services/stacked_services.dart';
+
+final GetIt locator = GetIt.instance;
+
+void setupLocator() {
+  locator.registerLazySingleton<UserRepository>(
+    () => UserRepositoryImplementation(
+      httpService: locator<HttpService>(),
+    ),
+  );
+  locator.registerLazySingleton<SnackbarService>(
+    () => SnackbarService(),
+  );
+}
