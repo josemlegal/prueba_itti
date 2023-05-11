5f1abf90901788111af0721048c9cd2a70d1db2e
josemlegal <josemlegal97@gmail.com>
2023-05-11 11:57:05 -0400

    Add view export to barell

---
+++ lib/presentation/views/views.dart
@@ -0,0 +1,4 @@
+export 'package:prueba_itti/presentation/views/settings_view.dart';
+export 'package:prueba_itti/presentation/views/login_view.dart';
+export 'package:prueba_itti/presentation/views/user_details_view.dart';
+export 'package:prueba_itti/presentation/views/users_view.dart';
